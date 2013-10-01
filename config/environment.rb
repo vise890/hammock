d090a91f706a3bd8825ed940d9bf12439b09e6f0
Martino Visintin <vise890@gmail.com>
2013-10-01 00:41:36 -0700

    initialize mongoid on app start

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,10 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
+# Load MongoDB ORM
+mongoid_yml = File.expand_path('mongoid.yml', File.dirname(__FILE__))
+Mongoid.load!(mongoid_yml)
+
 # Require all the models
 Dir['./api/models/*.rb'].each { |model| require model }
 
