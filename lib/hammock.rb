2e46aab44f3891f6fed3d0d6098874c752fee8d4
Martino Visintin <vise890@gmail.com>
2013-10-02 01:07:45 -0700

    destroy action

--- lib/hammock.rb
+++ lib/hammock.rb
@@ -7,6 +7,15 @@ helpers do
       halt 404
     end
   end
+
+  def find_resource
+    begin
+      get_resource(params[:resource_name]).find(params[:id])
+    rescue
+      halt 404
+    end
+  end
+
 end
 
 before do
@@ -26,5 +35,11 @@ end
 # E.g. GET /coconuts/524a87a7e0f3fc1d2d000001
 # return cocounut with _id = 524a87a7e0f3fc1d2d000001
 get '/:resource_name/:id' do
-  get_resource(params[:resource_name]).find(params[:id]).to_json
+  find_resource.to_json
+end
+
+# E.g. DELETE /coconuts/524a87a7e0f3fc1d2d000001
+# delete cocounut with _id = 524a87a7e0f3fc1d2d000001
+delete '/:resource_name/:id' do
+  find_resource.destroy.to_json
 end
