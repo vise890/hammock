fd289ccaa845252effd7b3bd94b22be6439e9088
Martino Visintin <vise890@gmail.com>
2013-10-02 08:53:38 -0700

    improve root page

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 # You can add your routes here
 # (Or override hammock created ones)
 get '/' do
-  "Add your resources in /resources"
+  "Add your resources in /resources. Add yor routes in app.rb"
 end
